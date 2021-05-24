db6700f2c7213a86929d8f17c24f915cf4c6f7bd
rubayet170746 <rafirubayet75@gmail.com>
2021-05-24 09:18:11 +0600

    added 2 more

---
+++ 1337a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin >> t;
+
+
+    int a,b,c,d;
+    while(t--)
+    {
+        cin >> a >> b >> c >> d;
+
+        cout << b << " " << c << " " << c << endl;
+    }
+
+
+
+    return 0;
+}
