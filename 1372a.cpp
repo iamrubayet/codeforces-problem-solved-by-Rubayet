f3a282e8012c9371600d11c2bcea8a0f5e79fa2d
rubayet170746 <rafirubayet75@gmail.com>
2021-06-01 10:49:20 +0600

    added 1372a

---
+++ 1372a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    int n;
+
+    while(t--)
+    {
+        cin >> n;
+
+        for(int i=0;i<n;i++)
+        {
+            cout << "1" <<" " ;
+
+        }
+
+        cout << endl;
+
+
+
+    }
+    return 0;
+
+}
