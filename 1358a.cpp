45bc737f5ffe7551f2f8e1f458a1e5290b43f147
rubayet170746 <rafirubayet75@gmail.com>
2021-06-14 03:10:28 +0600

    added

--- 1358a.cpp
+++ 1358a.cpp
@@ -1,34 +1,12 @@
 #include <iostream>
-#include <bits/stdc++.h>
 
 using namespace std;
 
-
-int main()
-{
-    int t;
+int main() {
+    int t, n, m;
     cin >> t;
-    while(t--)
-    {
-      int n,m;
-
-      cin >> n >> m;
-
-      if(m%2==0)
-      {
-          cout << n*(m/2) << endl;
-      }
-      else
-      {
-          cout << n*((m-1)/2)+ceil((n+1)/2)<<endl;
-
-      }
-
-
+    while (t--) {
+        cin >> n >> m;
+        cout << (n * m + 1) / 2 << '\n';
     }
-
-
-
-
-    return 0;
 }
