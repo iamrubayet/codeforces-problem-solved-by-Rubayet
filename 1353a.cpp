6f739246c95fdfddddca78b257ab0fc54f775a14
rubayet170746 <rafirubayet75@gmail.com>
2021-06-21 06:09:09 +0600

    added 1353

---
+++ 1353a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main()
+{
+
+    int t;
+    cin >>t;
+
+    for(int i=0;i<t;i++)
+    {
+        int n,m;
+        cin >> n >> m;
+
+        cout << min(2,n-1)*m <<endl;
+
+
+
+    }
+
+    return 0;
+}
