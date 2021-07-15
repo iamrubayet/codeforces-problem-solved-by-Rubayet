1fc68c0cc8c36232804dcb54288968d9c741961a
rubayet170746 <rafirubayet75@gmail.com>
2021-07-16 02:28:30 +0600

    added

---
+++ 451a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    int n,m;
+    cin >> n >>m;
+
+    int i;
+
+    if(n>m)
+    {
+        i = m;
+    }
+    else
+    {
+        i=n;
+    }
+
+    if(i%2==0)
+    {
+        cout << "Malvika"<<endl;
+    }
+    else
+    {
+        cout << "Akshat" << endl;
+    }
+    return 0;
+}
+
