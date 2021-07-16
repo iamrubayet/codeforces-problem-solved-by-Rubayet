de5439795a087e2948b97d236fc54f48dea80984
rubayet170746 <rafirubayet75@gmail.com>
2021-07-17 03:22:42 +0600

    added 460a

---
+++ 460a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+
+    int result=(n-1)/(m-1);
+    cout << n+result <<endl;
+
+
+    return 0;
+}
