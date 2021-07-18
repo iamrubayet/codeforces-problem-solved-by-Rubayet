e76c270556f536931c7aaa8cbf1000f13795016f
rubayet170746 <rafirubayet75@gmail.com>
2021-07-19 03:32:59 +0600

    added one more

---
+++ 1186a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    int n,m,k;
+
+    cin >> n >> m >> k;
+
+    if(n <= m && n <=k)
+    {
+        cout << "yes" <<endl;
+    }
+    else{
+        cout<< "No" <<endl;
+    }
+    return 0;
+}
+
