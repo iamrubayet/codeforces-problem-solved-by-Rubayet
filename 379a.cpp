2e21f175d834d8f0b0874cea652355c14cc6fa9d
rubayet170746 <rafirubayet75@gmail.com>
2021-07-22 03:36:12 +0600

    added

---
+++ 379a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    int a,b,d,m;
+
+    cin >> a >> b;
+
+    int h = a;
+
+    while(a>=b)
+    {
+        d = a/b;
+        h+= d;
+        m= a%b;
+        a = d+m;
+    }
+
+    cout << h << endl;
+    return 0;
+}
