1ff3dab9294321dcdd33689e3755f76b123de24d
rubayet170746 <rafirubayet75@gmail.com>
2021-08-25 21:56:54 +0600

    added 1553a

---
+++ 1553a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+
+        cin >> n;
+        cout << (n+1)/10 <<endl;
+    }
+    return 0;
+}
