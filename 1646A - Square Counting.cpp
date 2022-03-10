be6f93145d89be15dbc46908fe3bd1036e87fcf4
rubayet170746 <rafirubayet75@gmail.com>
2022-03-11 04:55:26 +0600

    added 800

---
+++ 1646A - Square Counting.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main()
+{
+    int tt;
+    cin >> tt;
+    for(int i=0;i<tt;i++){
+        long long n,s;
+        cin >> n >> s;
+        cout <<(s/(n*n))<<endl;
+
+    }
+    return 0;
+}
+
