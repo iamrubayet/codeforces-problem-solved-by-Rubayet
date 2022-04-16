5b0e48fa6817d42ed075fccb18ff0f0ec5da6aa6
rubayet170746 <rafirubayet75@gmail.com>
2022-04-16 21:07:35 +0600

    added 188

---
+++ 1644-B. Anti-Fibonacci Permutation.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using ll = long long;
+using ld = long double;
+
+
+using namespace std;
+
+
+int main()
+{
+    int t=1;
+    cin >>t;
+    while(t--)
+    {
+        int n,i=0;
+        cin >> n;
+        vector<int> v;
+        for(int i=1;i<=n;i++)
+        {
+                v.push_back(i);
+
+        }
+
+        reverse(v.begin(),v.end());
+
+        for(auto it:v)
+        {
+            cout << it <<" ";
+
+        }
+        cout <<endl;
+
+        n--;
+        while(n--)
+        {
+                swap(v[i],v[i+1]);
+                for(auto it:v)
+                {
+                    cout <<it<<" ";
+
+                }
+                cout <<endl;
+                swap(v[i],v[i+1]);
+                i++;
+
+        }
+
+    }
+    return 0;
+}
