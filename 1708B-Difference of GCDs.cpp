ad99c4aa16b7e82eb3aea275810f1d84f74a9feb
rubayet170746 <rafirubayet75@gmail.com>
2022-07-17 21:52:32 +0600

    added

---
+++ 1708B-Difference of GCDs.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    ll t, n, l, r, i, j, c;
+    
+    cin>>t;
+    
+    for(;t--;)
+    {
+        cin>>n>>l>>r;
+        vector<ll> ans;
+        
+        c=1;
+        ans.push_back(l);
+        for(i=2; i<=n; i++)
+        {
+            if((l%i)!=0){
+                j=l/i;
+                
+                j=(j+1)*i;
+                
+                if(j>=l && j<=r){
+                    c++;
+                    ans.push_back(j);
+                }else{
+                    break;
+                }
+            }else{
+                ans.push_back(l);
+                c++;
+            }
+        }
+        
+        if(c==n){
+            cout<<"YES\n";
+            for(i=0; i<n; i++){
+                cout<<ans[i]<<" ";
+            }
+            cout<<"\n";
+        }else{
+            cout<<"NO\n";
+        }
+        
+    }
+}
