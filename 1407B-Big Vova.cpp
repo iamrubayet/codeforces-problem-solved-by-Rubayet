d326c013e598c43efd3c5dd106aa1dbc0c98644d
rubayet170746 <rafirubayet75@gmail.com>
2022-12-02 18:57:05 +0600

    added

--- 1407B-Big Vova.cpp
+++ 1407B-Big Vova.cpp
@@ -4,30 +4,37 @@ using namespace std;
 using ll = long long;
 using ld = long double;
 
+//solution 2
+
 void solve()
 {
     int n;
     cin >> n;
     vector<int> a(n),b(n),vis(n);
+
     for(int i=0;i<n;i++)
     {
         cin >> a[i];
     }
-    int d=0;
+
+    int d = 0;
+
 
     for(int i=0;i<n;i++)
     {
         int index = 0;
-        int maxi = 0;
+        int maxigcd = 0;
+
         for(int j=0;j<n;j++)
         {
-            if(!vis[j] && __gcd(d,a[j])>maxi)
+            if(!vis[j] && __gcd(d,a[j])>maxigcd)
             {
-                maxi = __gcd(d,a[j]);
-                index= j;
+                maxigcd = __gcd(d,a[j]);
+                index = j;
+
             }
         }
-        d = maxi;
+        d = maxigcd;
         vis[index]=1;
         b[i]=a[index];
 
@@ -37,7 +44,9 @@ void solve()
     {
         cout << i << " ";
     }
-    cout << endl;
+    cout <<endl;
+
+
 
 
 }
